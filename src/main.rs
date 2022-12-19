55ae8a0b68b9935439bc5d98b09ef13db1352f0a
TheKonamiKoder <thekonamicode02@gmail.com>
2022-12-19 14:59:22 +0000

    Added the Jot struct A 'Jot' will be the struct which holds the information needed.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::time::{SystemTime};
+
+struct Jot {
+    short:String,
+    detailed:Option<String>,
+    timestamp:SystemTime
+}
+
+impl Jot {
+    fn new(short: String, detailed: Option<String>, timestamp: SystemTime) -> Self { Self { short, detailed, timestamp } }
+}
+
 fn main() {
     println!("Hello, world!");
 }
