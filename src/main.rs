b3ca85a4ec288e43d08176c735b5225a2802044f
TheKonamiKoder <thekonamicode02@gmail.com>
2022-12-19 15:44:55 +0000

    Formatted the timestamp

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,8 @@ fn main() {
     stdin().read_line(&mut short)
         .ok()
         .expect("Failed to get input...");
-    
+
     let jot = Jot::new(short, None, timestamp);
 
-    println!("{}\n{:?}\n{:?}", jot.short, jot.detailed, jot.timestamp)
+    println!("{}\n{:?}\n{}", jot.short, jot.detailed, jot.timestamp.format("[%d-%m-%Y %H:%M:%S]"));
 }
