9027fabd78ea4437bc6ee57da752d7e692259b99
TheKonamiKoder <thekonamicode02@gmail.com>
2022-12-21 14:59:27 +0000

    Added functionality to store to a json file

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,73 @@
 use std::env;
 use std::fs;
 use chrono::{DateTime, Local};
-use serde::{Deserialize};
+use rand::Rng;
+use serde::{Serialize, Deserialize};
+use serde_json;
+use rand;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct Jot {
     short:String,
     detailed:Option<String>,
     timestamp:String,
-    id:i64
+    id:u64
 }
 
 impl Jot {
-    fn new(short: String, detailed: Option<String>, timestamp: DateTime<Local>) -> Self { Self { short, detailed, timestamp:timestamp.format("[%d-%m-%Y %H:%H:%S]").to_string(), id:0 } }
+    fn new(short: String, detailed: Option<String>, timestamp: String, id:u64) -> Self { 
+        Self { 
+            short, 
+            detailed, 
+            timestamp,
+            id 
+        } 
+    }
+
+    fn gen_timestamp() -> String {
+        Local::now().format("%d-%m-%Y %H:%M:%S").to_string()
+    }
+    
+    fn gen_random_id(jots:&Vec<Jot>) -> u64 {
+        loop {
+            let id = rand::thread_rng().gen::<u64>();
+    
+            for jot in jots.iter() {
+                if jot.id == id {
+                    return id;
+                }
+            }
+        }
+    }
 }
 
-fn main() {
-    
+const JOTS_STORAGE_FILE:&str = "jots.json";
 
-    let timestamp = Local::now();
+fn main() {
     let args: Vec<String> = env::args().collect();
-    
     let cmd = &args[1];
 
-    let jot:Option<Jot> = match cmd.as_str() {
-        "new" => Some(Jot::new(args[2].as_str().to_string(), None, timestamp)),
-        _ => None
-    };
+    let mut jots:Vec<Jot> = serde_json::from_str(
+        &std::fs::read_to_string(JOTS_STORAGE_FILE).unwrap()
+    ).expect("There was an error with opening the JSON file...");
 
-    match jot {
-        Some(jot) => println!("Timestamp: {}\nShort: {}\nDetailed: {:?}", jot.timestamp, jot.short, jot.detailed),
-        None => println!("JOT NOT INITIALIZED CORRECTLY")
+    match cmd.as_str() {
+        "new" => {
+            jots.push(
+                Jot::new(
+                    args[2].clone(),
+                    None,
+                    Jot::gen_timestamp(),
+                    Jot::gen_random_id(&jots)
+                )
+            );
+        },
+        _ => println!("{} not a valid command!", cmd)
     }
+
+    std::fs::write(
+        JOTS_STORAGE_FILE, 
+        serde_json::to_string_pretty(&jots).unwrap()
+    ).unwrap();
+
 }
